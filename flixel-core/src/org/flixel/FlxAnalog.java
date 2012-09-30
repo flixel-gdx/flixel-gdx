caa6ac56756c817d2d3793a0373adff8f6862743
WingEraser <kawingchin@gmail.com>
2012-09-30 14:16:38 +0000

    Updated - FlxAnalog: entirely rewritten. It drags like a circle and the touch zone is a circle. Added method getAngle() which returns the angle in degrees. Callbacks added for pressed, justPressed, justReleased. It uses the AFlxButton. New skin for the analog base and stick.
    - FlxGamePad: new DPAD and Action Buttons. They are smaller. The position of the buttons are placed on a better place.
    
    - FlxU: math functions sin, cos and rint.

--- flixel-core/src/org/flixel/FlxAnalog.java
+++ flixel-core/src/org/flixel/FlxAnalog.java
@@ -1,9 +1,13 @@
 package org.flixel;
 
+import org.flixel.event.AFlxButton;
+
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 
 /**
- * 
+ * An analog stick or thumbstick with callbacks. It can easily be customized by 
+ * overriding the parent methods.
  * 
  * @author Ka Wing Chin
  */
@@ -12,96 +16,287 @@ public class FlxAnalog extends FlxGroup
 	// Base image
 	private final String ImgBase = "org/flixel/data/pack:base";
 	// Stick image
-	private final String ImgStick = "org/flixel/data/pack:stick";
+	private final String ImgThumb = "org/flixel/data/pack:stick";
 	
-	// How fast the speed of this object is changing.	
-	private static final float ACCELERATION = 10;
-	
-	public float x;
-	public float y;
-	private float _centerX;
-	private float _centerY;
+	// From radians to degrees.
+	private final double DEGREES = (180 / Math.PI);
 
-	private Rectangle _pad;
-	private FlxSprite _base;
-	public FlxSprite _stick;
+	// Used with public variable <code>status</code>, means not highlighted or pressed.
+	private final int NORMAL = 0;
+	// Used with public variable <code>status</code>, means highlighted (usually from mouse over).
+	private final int HIGHLIGHT = 1;
+	// Used with public variable <code>status</code>, means pressed (usually from mouse click).
+	private final int PRESSED = 2;		
+	// Shows the current state of the button.
+	public int status;
 	
-	public FlxPoint accel;
-	public boolean pressed;
-	private float yMin;
-	private float yMax;
-	private float xMin;
-	private float xMax;
+	// X position of the upper left corner of this object in world space.
+	public float x;
+	// Y position of the upper left corner of this object in world space.
+	public float y;
+	
+	// This function is called when the button is released.
+	public AFlxButton onUp;
+	// This function is called when the button is pressed down.
+	public AFlxButton onDown;
+	// This function is called when the mouse goes over the button.
+	public AFlxButton onOver;
+	// This function is called when the button is hold down.
+	public AFlxButton onPressed;
+	
+	// The area which the joystick will react.
+	private Circle _zone;
+	// The background of the joystick, also known as the base.
+	public FlxSprite bg;
+	// The thumb 
+	public FlxSprite thumb;
+	
+	// The radius where the thumb can move.
+	private float _radius;
+	private double _direction;
+	private float _amount;		
+	
+	// How fast the speed of this object is changing.
+	public FlxPoint acceleration;
+	// The speed of easing when the thumb is released.
+	private float _ease;
 		
 	/**
 	 * Constructor
-	 * @param X		The x-position of the analog stick
-	 * @param Y		The y-position of the analog stick.
+	 * @param	X		The X-coordinate of the point in space.
+ 	 * @param	Y		The Y-coordinate of the point in space.
+ 	 * @param	radius	The radius where the thumb can move. If 0, the background will be use as radius.
+ 	 * @param	ease	The duration of the easing. The value must be between 0 and 1.
 	 */
-	public FlxAnalog(float X, float Y)
+	public FlxAnalog(float x, float y, float radius, float ease)
 	{
-		accel  = new FlxPoint();
-		x = X;
-		y = Y;		
+		this.x = x;
+		this.y = y;			
+		_radius = radius;
+		_ease = ease;
 		
-		yMin = 0-24+y;
-		yMax = 100-24+y;
-		xMin = 0-24+x;
-		xMax = 100-24+x;
+		status = NORMAL;
+		_direction = 0;
+		_amount = 0;
+		acceleration = new FlxPoint();
 		
-		_pad = new Rectangle(x,y,100,100);
-		_centerX = X+_pad.width*.25f;
-		_centerY = Y+_pad.width*.25f;
-		
-		_base = new FlxSprite(X,Y, ImgBase);
-		//_base.setAlpha(0.75f);
-		_base.setSolid(false);
-		_base.ignoreDrawDebug = true;
-		add(_base);
-		
-		_stick = new FlxSprite(_centerX, _centerY, ImgStick);
-		_stick.width = _stick.height = 48;
-		_stick.offset.x = 20; 
-		_stick.offset.y = 3;
-		_stick.setSolid(false);
-		_stick.ignoreDrawDebug = true;
-		add(_stick);
+		createBase();
+		createThumb();
+		createZone();
+	}
+	
+	/**
+	 * Constructor
+	 * @param	X		The X-coordinate of the point in space.
+ 	 * @param	Y		The Y-coordinate of the point in space.
+ 	 * @param	radius	The radius where the thumb can move. If 0, the background will be use as radius.
+	 */
+	public FlxAnalog(float x, float y, float radius)
+	{
+		this(x, y, radius, .25f);
+	}
+	
+	/**
+	 * Constructor
+	 * @param	X		The X-coordinate of the point in space.
+ 	 * @param	Y		The Y-coordinate of the point in space.
+	 */
+	public FlxAnalog(float x, float y)
+	{
+		this(x, y, 0, .25f);
+	}
+	
+	/**
+	 * Constructor
+	 * @param	X		The X-coordinate of the point in space.
+	 */
+	public FlxAnalog(float x)
+	{
+		this(x, 0, 0, .25f);
+	}
+	
+	/**
+	 * Constructor
+	 */
+	public FlxAnalog()
+	{
+		this(0, 0, 0, .25f);
 	}
 	
 	
+	/**
+	 * Creates the background of the analog stick.
+	 * Override this to customize the background.
+	 */
+	protected void createBase()
+	{
+		bg = new FlxSprite(x, y).loadGraphic(ImgBase);
+		bg.x += -bg.width * .5;
+		bg.y += -bg.height * .5;
+		bg.scrollFactor.x = bg.scrollFactor.y = 0;
+		bg.setSolid(false);
+		bg.ignoreDrawDebug = true;
+		add(bg);			
+	}
+	
+	/**
+	 * Creates the thumb of the analog stick.
+	 * Override this to customize the thumb.
+	 */
+	protected void createThumb()
+	{
+		thumb = new FlxSprite(x, y).loadGraphic(ImgThumb);
+		thumb.scrollFactor.x = thumb.scrollFactor.y = 0;
+		thumb.setSolid(false);
+		thumb.ignoreDrawDebug = true;
+		add(thumb);
+	}
+	
+	/**
+	 * Creates the touch zone. It's based on the size of the background. 
+	 * The thumb will react when the mouse is in the zone.
+	 * Override this to customize the zone.
+	 * 
+	 * @param	contract	Contract the size.
+	 */
+	protected void createZone()
+	{
+		if(_radius == 0)			
+			_radius = bg.width / 2;
+		_zone = new Circle(x, y, _radius);
+	}
+	
+	/**
+	 * Clean up memory.
+	 */
+	@Override
+	public void destroy()
+	{
+		super.destroy();
+		onUp = onDown = onOver = onPressed = null;
+		acceleration = null;
+		thumb = null;
+		_zone = null;
+		bg = null;
+		thumb = null;
+	}
+	
+	
+	/**
+	 * Update the behaviour. 
+	 */
 	@Override
 	public void update()
 	{
-		if(FlxG.mouse.pressed())
+		boolean offAll = true;
+		
+		if(_zone.contains(FlxG.mouse.screenX, FlxG.mouse.screenY) || status == PRESSED)
 		{
-			if(_pad.contains(FlxG.mouse.screenX, FlxG.mouse.screenY) || pressed)
+			offAll = false;				
+			
+			if(FlxG.mouse.pressed())
 			{
-				pressed = true;
-				_stick.x = FlxG.mouse.x - _stick.width*.5f;
-				_stick.y = FlxG.mouse.y - _stick.height*.5f;
+				status = PRESSED;	
 				
-				if(_stick.y <= yMin)
-					_stick.y = yMin;
-				if(_stick.y >= yMax)
-					_stick.y = yMax;
-				if(_stick.x <= xMin)
-					_stick.x = xMin;
-				if(_stick.x >= xMax)
-					_stick.x = xMax;
-				
-				accel.x = ((74 - (100 - (_stick.x)))-x) / ACCELERATION;
-				accel.y = ((74 - (100 - (_stick.y)))-y) / ACCELERATION;
+				if(FlxG.mouse.justPressed())
+				{
+					if(onDown != null)
+						onDown.callback();
+				}
+							
+				if(status == PRESSED)
+				{
+					if(onPressed != null)
+						onPressed.callback();						
+					
+					float dx = FlxG.mouse.screenX-x;
+					float dy = FlxG.mouse.screenY-y;
+	
+					double dist = Math.sqrt(dx * dx + dy * dy); // TODO: replace Math.sqrt, but need to benchmark this in Flash version 11.4.
+					if(dist < 1) 
+						dist = 0;
+					_direction = Math.atan2(dy, dx); // TODO: replace Math.atan2, but need to benchmark this in Flash version 11.4.
+					_amount = FlxU.min(_radius, (float) dist) / _radius;
+							
+					acceleration.x = (float) (FlxU.cos(_direction) * _amount * _radius);
+					acceleration.y = (float) (FlxU.sin(_direction) * _amount * _radius);			
+				}					
+			}
+			else if(FlxG.mouse.justReleased() && status == PRESSED)
+			{				
+				status = HIGHLIGHT;
+				if(onUp != null)
+					onUp.callback();					
+				acceleration.x = 0;
+				acceleration.y = 0;
+			}			
+			
+			if(status == NORMAL)
+			{
+				status = HIGHLIGHT;
+				if(onOver != null)
+					onOver.callback();
 			}
 		}
-		else
+		if((status == HIGHLIGHT || status == NORMAL) && _amount != 0)
+		{				
+			_amount *= _ease;
+			if(FlxU.abs(_amount) < 0.1) 
+				_amount = 0;				
+		}					
+		
+		thumb.x = (float) (x + FlxU.cos(_direction) * _amount * _radius - (thumb.width * .5f));
+		thumb.y = (float) (y + FlxU.sin(_direction) * _amount * _radius - (thumb.height * .5f));
+		
+		if(offAll)
 		{
-			pressed = false;			
-			_stick.x = _centerX -((_centerX - _stick.x) / 1.5f);
-			_stick.y = _centerY - ((_centerY - _stick.y) / 1.5f);
-			// TODO: add motion to accel when released.
-			accel.x = 0;//FlxU.round(((74 - (100 - (_stick.x)))-x) / ACCELERATION);
-			accel.y = 0;//FlxU.round(((74 - (100 - (_stick.y)))-y) / ACCELERATION);			
+			status = NORMAL;
 		}
 		super.update();
+	}
+	
+	/**
+	 * returns the angle in degrees.
+	 * @return	The angle.
+	 */
+	public float getAngle()
+	{
+		return (float) (Math.atan2(acceleration.y,acceleration.x) * DEGREES);
+	}
+	
+	/**
+	 * Whether the thumb is pressed or not.
+	 */
+	public boolean pressed()
+	{
+		return status == PRESSED;
+	}
+	
+	/**
+	 * Whether the thumb is just pressed or not.
+	 */
+	public boolean justPressed()
+	{
+		return FlxG.mouse.justPressed() && status == PRESSED;
+	}
+	
+	/**
+	 * Whether the thumb is just released or not.
+	 */
+	public boolean justReleased()
+	{
+		return FlxG.mouse.justReleased() && status == HIGHLIGHT;
+	}
+
+	/**
+	 * Set <code>alpha</code> to a number between 0 and 1 to change the opacity of the gamepad.
+	 * @param Alpha
+	 */
+	public void setAlpha(float Alpha)
+	{
+		for(int i = 0; i < members.size; i++)
+		{
+			((FlxSprite) members.get(i)).setAlpha(Alpha);
+		}
 	}
 }
