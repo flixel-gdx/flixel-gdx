f935e557a052923bd9379db20a52dc64167fa3fd
WingEraser <kawingchin@gmail.com>
2014-01-16 19:20:24 +0100

    Added the rest of the classes in .gwt.xml

--- flixel-html5/src/org/flixel/client/FlxHtml5Application.java
+++ flixel-html5/src/org/flixel/client/FlxHtml5Application.java
@@ -5,6 +5,16 @@ import org.flixel.FlxGame;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader.PreloaderCallback;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader.PreloaderState;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class FlxHtml5Application extends GwtApplication
 {
@@ -29,5 +39,40 @@ public class FlxHtml5Application extends GwtApplication
 	public ApplicationListener getApplicationListener()
 	{
 		return (ApplicationListener)_game.stage;
+	}
+	
+	@Override
+	public PreloaderCallback getPreloaderCallback()
+	{
+		final Panel preloaderPanel = new VerticalPanel();
+		preloaderPanel.setStyleName("gdx-preloader");
+		final Image logo = new Image(GWT.getHostPageBaseURL() + "assets/org/flixel/client/logo.png");
+		logo.setStyleName("logo");		
+		preloaderPanel.add(logo);
+		final Panel meterPanel = new SimplePanel();
+		meterPanel.setStyleName("gdx-meter");
+		meterPanel.addStyleName("red");
+		final InlineHTML meter = new InlineHTML();
+		final Style meterStyle = meter.getElement().getStyle();
+		meterStyle.setWidth(0, Unit.PCT);
+		meterPanel.add(meter);
+		preloaderPanel.add(meterPanel);
+		getRootPanel().add(preloaderPanel);
+		
+		return new PreloaderCallback()
+		{
+			
+			@Override
+			public void update(PreloaderState state)
+			{
+				meterStyle.setWidth(100f * state.getProgress(), Unit.PCT);
+			}
+			
+			@Override
+			public void error(String file)
+			{
+				System.out.println("error: " + file);
+			}
+		};
 	}
 }
