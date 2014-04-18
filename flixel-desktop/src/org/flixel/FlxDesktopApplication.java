bc0b97bdaa76e5d074f62961ef4fee682553205e
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@xamarin.com>
2014-04-18 23:23:10 +0200

    desktop: deprecated desktop app constructos that takes optional gl20 support arg

--- flixel-desktop/src/org/flixel/FlxDesktopApplication.java
+++ flixel-desktop/src/org/flixel/FlxDesktopApplication.java
@@ -5,18 +5,31 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class FlxDesktopApplication extends LwjglApplication
 {
+    /**
+     * Use variant without UseGL2 argument. GL2 is required since libgdx 1.0.
+     */
+    @Deprecated
 	public FlxDesktopApplication(FlxGame Game, int Width, int Height, boolean UseGL2)
 	{
-		super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height, UseGL2);
+		super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height);
 	}
 	
 	public FlxDesktopApplication(FlxGame Game, int Width, int Height)
 	{
-		this(Game, Width, Height, false);
+        super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height);
 	}
-	
+
+    public FlxDesktopApplication(FlxGame Game, String title, int Width, int Height)
+    {
+        super((ApplicationListener)Game.stage, title, Width, Height);
+    }
+
+    /**
+     * Use variant without UseGL2 argument. GL2 is required since libgdx 1.0.
+     */
+    @Deprecated
 	public FlxDesktopApplication(FlxGame Game, String title, int Width, int Height, boolean UseGL2)
 	{
-	    super((ApplicationListener)Game.stage, title, Width, Height, UseGL2);
+	    super((ApplicationListener)Game.stage, title, Width, Height);
 	}
 }
