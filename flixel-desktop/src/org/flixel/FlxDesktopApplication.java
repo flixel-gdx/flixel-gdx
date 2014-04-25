8ec3a2a754c3074cde79a8f79a6f5f25dfe11a44
WingEraser <kawingchin@gmail.com>
2014-04-25 23:06:04 +0200

    Removed some GLES20 checks.

--- flixel-desktop/src/org/flixel/FlxDesktopApplication.java
+++ flixel-desktop/src/org/flixel/FlxDesktopApplication.java
@@ -5,31 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class FlxDesktopApplication extends LwjglApplication
 {
-    /**
-     * Use variant without UseGL2 argument. GL2 is required since libgdx 1.0.
-     */
-    @Deprecated
-	public FlxDesktopApplication(FlxGame Game, int Width, int Height, boolean UseGL2)
+   	public FlxDesktopApplication(FlxGame Game, int Width, int Height)
 	{
-		super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height);
-	}
-	
-	public FlxDesktopApplication(FlxGame Game, int Width, int Height)
-	{
-        super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height);
+        super((ApplicationListener)Game.stage, FlxG.getLibraryName(), Width, Height, true);
 	}
 
     public FlxDesktopApplication(FlxGame Game, String title, int Width, int Height)
     {
-        super((ApplicationListener)Game.stage, title, Width, Height);
+        super((ApplicationListener)Game.stage, title, Width, Height, true);
     }
-
-    /**
-     * Use variant without UseGL2 argument. GL2 is required since libgdx 1.0.
-     */
-    @Deprecated
-	public FlxDesktopApplication(FlxGame Game, String title, int Width, int Height, boolean UseGL2)
-	{
-	    super((ApplicationListener)Game.stage, title, Width, Height);
-	}
 }
